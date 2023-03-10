8a433b43978b69f7cd45794280a0aee9757ea431
hideonbush3 <ppoii0961@gmail.com>
2023-03-10 13:15:49 +0900

    즐겨찾기 목록 클릭했을때 모달창 띄우고 즐겨찾기 목록 페이지와 회원정보 페이지의 CSS 수정

--- wakaut-react/src/components/favorites_list.js
+++ wakaut-react/src/components/favorites_list.js
@@ -1,20 +1,26 @@
 import {Button, Form} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import React, {useState} from "react";
+import PlaceModal from "../pages/place_modal";
+import img from "../assets/workaut.png";
 
 const FavoritesList = (props) => {
     const [lgShow, setLgShow] = useState(false);
+    let addr = '경기도 김포시 대평원거리 11-43';
+    let des = '#씨름 #강호동 #아는형님';
 
     return (
         <>
-        <Form className="d-flex shadow" style={{height:'100px', fontWeight:'bold', fontSize:'25px', textAlign:'center', alignItems: 'center', marginTop:'20px'}}>
+        <Form className="d-flex shadow" style={{height:'60px', fontSize:'12px', textAlign:'center', alignItems: 'center', marginTop:'10px'}}>
             <Form.Label column lg={1}>{props.region}</Form.Label>
             <Form.Label column lg={6}><Link to={'#'} onClick={()=>{ setLgShow(true)}} style={{textDecoration: 'none', color:'#17a1b8'}}>{props.gym}</Link></Form.Label>
             <Form.Label column lg={2}>{props.contact}</Form.Label>
             <Form.Label  column lg={3} style={{textAlign:'end'}}>
-                <Button type='button' style={{border:'none', padding: '0 0', color:'white', fontWeight:'bold', fontSize: '25px', backgroundColor: '#17a1b8', width: '40%', marginRight: '10px'}}>삭제</Button>
+                <Button type='button' style={{border:'none', height:'50%', color:'white', fontWeight:'bold', fontSize: '12px', backgroundColor: '#17a1b8', width: '30%', marginRight: '10px'}}>삭제</Button>
             </Form.Label>
         </Form>
+    <PlaceModal lgShow={lgShow} setLgShow={setLgShow} score={3} addr={addr} title={props.gym} des={des} img={img} />
+
         </>
     );
 }
