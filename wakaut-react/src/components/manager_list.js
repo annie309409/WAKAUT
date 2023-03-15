c60f3d3c48a1be9b303d8f9def857e96d14f899b
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-03-15 14:45:03 +0900

    회원 정보 관리 css 수정

--- wakaut-react/src/components/manager_list.js
+++ wakaut-react/src/components/manager_list.js
@@ -5,13 +5,13 @@ import Mgmodal from "./mgmodal";
 const Managerlist = (props)=>{
     return(
         <div className="shadow mt-8">
-            <div className="article d-flex p-3" style={{margin:"20px"}}>
+            <div className="article d-flex p-3" >
                 <div className="left d-flex col-12" >
-                    {(props.img)? (<div className="image" style={{padding: '50px',margin:'5px',borderRadius:'20px',width:'200px' }}>프로필사진</div>):(<div className="image">프로필 사진이 없습니다.</div>)}
-                    <div className="texts ms-2 col-8" style={{display:"flex"}}>
-                        <div style={{display:'inline', marginLeft:"15px"}}>
-                            <ul style={{listStyle:'none'}}>
-                                <li style={{color:"gray"}}><p>회원 정보</p></li>
+                    {(props.img)? (<div className="accountimg">프로필사진</div>):(<div className="accountimg">프로필 사진이 없습니다.</div>)}
+                    <div className="texts ms-2 col-8 standal" >
+                        <div className="accountinfo">
+                            <ul className="accountTitle">
+                                <li ><p>회원 정보</p></li>
                                 <li>이름 {props.name}</li>
                                 <li>성별 {props.gender}</li>
                                 <li>생년월일 {props.birth}</li>
@@ -19,16 +19,16 @@ const Managerlist = (props)=>{
                                 <li>이메일 {props.email}</li>
                             </ul>
                         </div>
-                        <div style={{display:'inline', marginLeft:"45px"}}>
-                            <ul style={{listStyle:'none'}}>
+                        <div className="accountinfo2">
+                            <ul className="accountTitle">
                                 <li style={{color:"gray"}}><p>게시판</p></li>
                                 <li>게시글수 {props.boardnum}개</li>
                                 <li>댓글 수 {props.comments}개</li>
                             </ul>
                         </div>
                     </div>
-                    <div style={{display:'flex',justifyContent:"end",alignContent:"center",alignItems:"end"  }} >
-                        <Button type='button' onClick={()=>{props.setLgShow(true)}} style={{background:'#239aaf', border:'none', color:'white', fontSize: '20px'}}>회원 사용 정지</Button>
+                    <div className="stopbtn" >
+                        <Button type='button' className="acstopbtn" onClick={()=>{props.setLgShow(true)}} >회원 사용 정지</Button>
                     </div>
                 </div>
             </div>
