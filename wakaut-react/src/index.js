fafe79f7206cf7a8977c73e85877c7db4683e9a7
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-03-15 14:32:47 +0900

    회원 정보 관리 css 수정

--- wakaut-react/src/index.js
+++ wakaut-react/src/index.js
@@ -9,10 +9,13 @@ import {BrowserRouter as Router} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Header menu={['mypage','community','login/signin']} route={['/mypage','/boardlist','/login']}/>
+      <Header menu={['mypage','community','login/signin','manager']} route={['/mypage','/boardlist','/login','/manager']}/>
       <App />
       <Footer />
     </Router>
   </React.StrictMode>,document.getElementById('root')
 );
+
+
+
 
