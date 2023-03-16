5cfeb62127443ab4b6a0d61ea26f6c44a9ce9720
annie309409 <pjatopaz1@naver.com>
2023-03-16 12:47:40 +0900

    next.config수정

--- next/next.config.js
+++ next/next.config.js
@@ -1,12 +1,9 @@
 withImages = require('next-images');
 
-// const nextConfig={
-//     reactStrictMode: true,
-//     // 국제화 : 그 나라에 맞게 문자를 국제화하는것 (internationalization)
-//     i18n : {
-//         locales:['ko'], defaultLocale : 'ko'
-//     } 
-// };
-
-// module.exports = {nextConfig, withImages};
-module.exports = withImages();
+module.exports = withImages({
+    reactStrictMode: true,
+    // 국제화 : 그 나라에 맞게 문자를 국제화하는것 (internationalization)
+    i18n : {
+        locales:['ko'], defaultLocale : 'ko'
+    } 
+});
